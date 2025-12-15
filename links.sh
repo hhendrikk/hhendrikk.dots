5f9eacf78a24b6af8f3f779a47e55ee5ac952e11
Hendrik Mourão <jhendrik@gmail.com>
2025-12-15 04:32:38 -0300

    add links.sh

---
+++ links.sh
@@ -0,0 +1,7 @@
+ln -s ~/hhendrikk.dots/starship.toml ~/.config/starship.toml
+ln -s ~/hhendrikk.dots/alacritty ~/.config/alacritty
+ln -s ~/hhendrikk.dots/niri/niri.kdl ~/.config/niri/config.kdl
+ln -s ~/hhendrikk.dots/.bashrc.my ~/.bashrc.my
+ln -s ~/hhendrikk.dots/.gitconfig ~/.gitconfig
+
+echo ". ~/.bashrc.my" >> ~/.bashrc
