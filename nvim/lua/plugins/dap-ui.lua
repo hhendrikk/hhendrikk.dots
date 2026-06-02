9e13e813fe8818530b55504ee7162642d131160f
Hendrik Araujo <jhendrik@gmail.com>
2026-06-02 14:31:30 -0300

    Refactor DAP UI configuration and add DAP view

--- nvim/lua/plugins/dap-ui.lua
+++ nvim/lua/plugins/dap-ui.lua
@@ -1,50 +1,77 @@
 return {
-  "rcarriga/nvim-dap-ui",
-  dependencies = { "mfussenegger/nvim-dap", "nvim-neotest/nvim-nio" },
-  opts = {
-    floating = {
-      max_height = 0.9,
-      max_width = 0.5,
-      border = "rounded",
-      mappings = {
-        close = { "q", "<Esc>" },
-      },
-    },
-    layouts = {
-      {
-        elements = {
-          { id = "scopes", size = 0.25 },
-          { id = "breakpoints", size = 0.25 },
-          { id = "stacks", size = 0.25 },
-          { id = "watches", size = 0.25 },
+  { import = "lazyvim.plugins.extras.dap.core" },
+
+  -- desativa nvim-dap-ui
+  { "rcarriga/nvim-dap-ui", enabled = false },
+
+  -- nvim-dap principal
+  {
+    "mfussenegger/nvim-dap",
+    init = function()
+      vim.fn.sign_define("DapBreakpoint", {
+        text = "▌",
+        texthl = "DapBreakpoint",
+        linehl = "",
+        numhl = "",
+      })
+
+      vim.api.nvim_create_autocmd("ColorScheme", {
+        callback = function()
+          vim.api.nvim_set_hl(0, "DapBreakpoint", { fg = "#ff0000" })
+        end,
+      })
+
+      vim.api.nvim_set_hl(0, "DapBreakpoint", { fg = "#ff0000" })
+    end,
+
+    opts = function()
+      local dap = require("dap")
+
+      dap.adapters.coreclr = dap.adapters.coreclr
+        or {
+          type = "executable",
+          command = "netcoredbg",
+          args = { "--interpreter=vscode" },
+        }
+
+      local config = {
+        {
+          type = "coreclr",
+          name = "launch - netcoredbg",
+          request = "launch",
+          program = function()
+            return vim.fn.input("Path to dll: ", vim.fn.getcwd() .. "/bin/Debug/", "file")
+          end,
         },
-        position = "left",
-        size = 30,
-      },
-      {
-        elements = {
-          { id = "console", size = 1.0 },
-        },
-        position = "bottom",
-        size = 10,
-      },
-    },
+      }
+
+      dap.configurations.cs = config
+      dap.configurations.fsharp = config
+    end,
   },
-  config = function(_, opts)
-    local dapui = require("dapui")
-    dapui.setup(opts)
 
-    vim.keymap.set("n", "<leader>di", function()
-      require("dap.ui.widgets").hover()
-    end)
+  -- nvim-dap-view
+  {
+    "igorlfs/nvim-dap-view",
+    dependencies = { "mfussenegger/nvim-dap" },
+    opts = {},
+    keys = {
+      { "<leader>dv", "<cmd>DapViewToggle<cr>", desc = "Dap View Toggle" },
+      { "<leader>dV", "<cmd>DapViewOpen<cr>", desc = "Dap View Open" },
+      { "<leader>dc", "<cmd>DapViewClose<cr>", desc = "Dap View Close" },
+    },
+    config = function(_, opts)
+      require("dap-view").setup(opts)
 
-    vim.keymap.set("n", "<leader>dr", function()
-      dapui.float_element("repl", { enter = true, position = "center" })
-    end, { desc = "REPL Flutuante" })
+      local dap = require("dap")
 
-    local dap = require("dap")
-    dap.listeners.after.event_initialized["dapui_config"] = function()
-      dapui.open()
-    end
-  end,
+      dap.listeners.before.event_terminated["dap_view_close"] = function()
+        pcall(vim.cmd, "DapViewClose")
+      end
+
+      dap.listeners.before.event_exited["dap_view_close"] = function()
+        pcall(vim.cmd, "DapViewClose")
+      end
+    end,
+  },
 }
