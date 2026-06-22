40c4ba29d1ff4371ef23ae1236eb965ed25d8fb3
Hendrik Mourão <hendrik@neocom.info>
2026-06-21 22:13:34 -0300

    chore: configs to almalinux without niri

--- links.sh
+++ links.sh
@@ -1,6 +1,4 @@
 ln -s ~/hhendrikk.dots/starship.toml ~/.config/starship.toml
-ln -s ~/hhendrikk.dots/alacritty ~/.config/alacritty
-ln -s ~/hhendrikk.dots/niri/niri.kdl ~/.config/niri/config.kdl
 ln -s ~/hhendrikk.dots/fish/config.fish ~/.config/fish/config.fish
 ln -s ~/hhendrikk.dots/.gitconfig ~/.gitconfig
 ln -s ~/hhendrikk.dots/nvim ~/.config/nvim
